545995af123b6ad619e1a62f84620b8b48b38b04
Wang Qi <wangqi@wangs-air.mynet>
2021-03-13 21:03:49 +0000

    add levels

--- AlienInvasion/settings.py
+++ AlienInvasion/settings.py
@@ -8,19 +8,31 @@ class Settings:
         self.bg_color = (230, 230, 230)
         
         # Ship settings
-        self.ship_speed = 5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 20
 
         # Alien settings
-        self.alien_speed = 10.0
         self.fleet_drop_speed = 30
+        self.speedup_scale = 1.2
 
-        # 1 -> right, -1 -> left
+        self.init_dynamic_settings()
+       
+
+    def init_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 5
+        self.bullet_speed = 5
+        self.alien_speed = 5
+
+         # 1 -> right, -1 -> left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
